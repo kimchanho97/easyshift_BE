5a9f4e554c5f7176ee7f18ad2c3e7535652a2ded
YeongJae0114 <yjl8628@naver.com>
2025-02-18 02:10:30 +0900

    임시 repository 생성

--- src/main/java/com/burntoburn/easyshift/repository/schedule/ScheduleRepository.java
+++ src/main/java/com/burntoburn/easyshift/repository/schedule/ScheduleRepository.java
@@ -1,4 +1,4 @@
-package com.burntoburn.easyshift.repository;
+package com.burntoburn.easyshift.repository.schedule;
 
 import com.burntoburn.easyshift.entity.schedule.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
---
+++ src/main/java/com/burntoburn/easyshift/repository/store/StoreRepository.java
@@ -0,0 +1,9 @@
+package com.burntoburn.easyshift.repository.store;
+
+import com.burntoburn.easyshift.entity.store.Store;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StoreRepository extends JpaRepository<Store, Long> {
+}
