262a58b11749132fae684ee747272c0f209efe1e
s1uth <handtaein@gmail.com>
2025-02-19 11:05:52 +0900

    feat: User Name 필드 추가, 기가입자 검증

--- src/main/java/com/burntoburn/easyshift/service/UserService.java
+++ src/main/java/com/burntoburn/easyshift/service/UserService.java
@@ -1,6 +1,6 @@
 package com.burntoburn.easyshift.service;
 
-import com.burntoburn.easyshift.dto.AddUserRequest;
+import com.burntoburn.easyshift.dto.user.AddUserRequest;
 import com.burntoburn.easyshift.entity.user.User;
 import com.burntoburn.easyshift.repository.user.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -13,9 +13,14 @@ public class UserService  {
     private final UserRepository userRepository;
 
 
-    public User save(AddUserRequest request) {
+    public User save(AddUserRequest request) throws Exception{
+        if (userRepository.findByEmail(request.getEmail()).isPresent()) {
+            throw new Exception("이미 존재하는 계정 입니다.");
+        }
+
         User newUser = User.builder()
                 .email(request.getEmail())
+                .name(request.getName())
                 .phoneNumber(request.getPhoneNumber())
                 .role(request.getRole())
                 .avatarUrl(request.getAvatarUrl())
