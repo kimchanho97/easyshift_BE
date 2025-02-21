60677955263bb7dd41f2a03a8e5d5c3203237776
s1uth <handtaein@gmail.com>
2025-02-21 21:15:53 +0900

    feat: token filter 생성

---
+++ src/main/java/com/burntoburn/easyshift/config/jwt/TokenAuthenticationFilter.java
@@ -0,0 +1,39 @@
+package com.burntoburn.easyshift.config.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class TokenAuthenticationFilter extends OncePerRequestFilter {
+
+    private final TokenProvider tokenProvider;
+    private final static String HEADER_AUTHORIZATION = "Authorization";
+    private final static String TOKEN_PREFIX = "Bearer";
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        String autholrizationHeader = request.getHeader("HEADER_AUTHORIZATION");
+        String accessToken = getAccessToken(autholrizationHeader);
+        // access token 을 검증하고 유효하다면 인증정보를 security context에 저장
+        if (tokenProvider.validToken(accessToken)) {
+            Authentication authentication = tokenProvider.getAuthentication(accessToken);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+    }
+
+    private String getAccessToken(String autholrizationHeader) {
+        if (autholrizationHeader != null && autholrizationHeader.startsWith("TOKEN_PREFIX")) {
+            return autholrizationHeader.substring(TOKEN_PREFIX.length());
+        }
+        return null;
+    }
+}
