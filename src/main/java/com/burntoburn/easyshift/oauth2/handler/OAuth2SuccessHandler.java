97d99db5337e05d7ffedb73cfb576c6e72315dbf
s1uth <handtaein@gmail.com>
2025-02-21 21:15:35 +0900

    feat: Access Token -> RefreshToken 변경, 추가

--- src/main/java/com/burntoburn/easyshift/oauth2/handler/OAuth2SuccessHandler.java
+++ src/main/java/com/burntoburn/easyshift/oauth2/handler/OAuth2SuccessHandler.java
@@ -1,9 +1,9 @@
 package com.burntoburn.easyshift.oauth2.handler;
 
 import com.burntoburn.easyshift.config.jwt.TokenProvider;
-import com.burntoburn.easyshift.entity.user.Token;
+import com.burntoburn.easyshift.entity.user.RefreshToken;
 import com.burntoburn.easyshift.entity.user.User;
-import com.burntoburn.easyshift.repository.user.AccessTokenRepository;
+import com.burntoburn.easyshift.repository.user.RefreshTokenRepository;
 import com.burntoburn.easyshift.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -26,21 +26,22 @@ import java.util.Map;
 public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
     private final TokenProvider tokenProvider;
-    private final AccessTokenRepository accessTokenRepository;
+    private final RefreshTokenRepository refreshTokenRepository;
     private final UserService userService;
 
-    // public static final Duration REFRESH_TOKEN_DURATION = Duration.ofDays(14);
+    public static final Duration REFRESH_TOKEN_DURATION = Duration.ofDays(14);
     public static final Duration ACCESS_TOKEN_DURATION = Duration.ofDays(2);
     // 프론트엔드 콜백 페이지 URL ( 추후 수정 예정 )
     public static final String FRONTEND_CALLBACK_URL = "http://localhost:3000/auth/callback";
 
     @Override
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
-                                        Authentication authentication) throws IOException {
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
+
         log.info("OAuth2 Login 성공!");
         OAuth2AuthenticationToken oauthToken = (OAuth2AuthenticationToken) authentication;
         OAuth2User oAuth2User = oauthToken.getPrincipal();
 
+        // Oauth2를 통해 불러온 유저 정보
         Map<String, Object> attributes = oAuth2User.getAttributes();
         Map<String,Object> kakaoAccount = (Map<String,Object>) attributes.get("kakao_account");
         Map<String,Object> profile = (Map<String,Object>) kakaoAccount.get("profile");
@@ -68,8 +69,9 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
 
         // 자체 access token 생성
         String accessToken = tokenProvider.generateToken(user, ACCESS_TOKEN_DURATION);
-
-        saveToken(user.getId(), accessToken);
+        // 리프레시 토큰 생성 (OAuth 액세스 토큰은 포함하지 않음)
+        String refreshToken = tokenProvider.generateToken(user, REFRESH_TOKEN_DURATION);
+        saveToken(user.getId(), refreshToken);
 
         // 로그인 성공 메시지와 access token 을 반환
         String targetUrl = UriComponentsBuilder.fromUriString(FRONTEND_CALLBACK_URL)
@@ -78,15 +80,22 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
                 .build()
                 .toUriString();
 
+        clearAuthenticationAttributes(request, response);
         getRedirectStrategy().sendRedirect(request, response, targetUrl);
     }
 
-    // accessToken 이 DB에 있는지 확인하고 업데이트 하거나 저장
-    private void saveToken(Long userId, String newAccessToken) {
-        Token AccessToken = accessTokenRepository.findByUserId(userId)
-                .map(entity -> entity.update(newAccessToken))
-                .orElse(Token.builder().accessToken(newAccessToken).id(userId).build());
-        accessTokenRepository.save(AccessToken);
+    // RefreshToken 이 DB에 있는지 확인하고 업데이트 하거나 저장
+    private void saveToken(Long userId, String newRefreshToken) {
+        RefreshToken refreshToken = refreshTokenRepository.findByUserId(userId)
+                .map(entity -> entity.update(newRefreshToken))
+                .orElse(RefreshToken.builder().refreshToken(newRefreshToken).id(userId).build());
+        refreshTokenRepository.save(refreshToken);
     }
+
+    // 인증 관련 설정값을 제거
+    private void clearAuthenticationAttributes(HttpServletRequest request, HttpServletResponse response) {
+        super.clearAuthenticationAttributes(request);
+    }
+
 
 }
