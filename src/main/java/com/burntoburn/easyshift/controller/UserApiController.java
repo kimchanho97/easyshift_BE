f8bc1197c6b48103dbc1b97b317662940b69a6d3
s1uth <handtaein@gmail.com>
2025-02-23 16:39:56 +0900

    refactor: user service 리팩터링

--- src/main/java/com/burntoburn/easyshift/controller/UserApiController.java
+++ src/main/java/com/burntoburn/easyshift/controller/UserApiController.java
@@ -2,7 +2,7 @@ package com.burntoburn.easyshift.controller;
 
 import com.burntoburn.easyshift.dto.user.AddUserRequest;
 import com.burntoburn.easyshift.entity.user.User;
-import com.burntoburn.easyshift.service.UserService;
+import com.burntoburn.easyshift.service.user.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
