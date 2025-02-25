79795a0f98419910ea3e66863fda28d790106621
Juhyeong Ko <dury.ko@gmail.com>
2025-02-26 00:28:05 +0900

    feat: Swagger 설정 업데이트 (#28)

--- src/main/java/com/burntoburn/easyshift/config/SwaggerConfig.java
+++ src/main/java/com/burntoburn/easyshift/config/SwaggerConfig.java
@@ -6,28 +6,25 @@ import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
-
-/**
- * Swagger는 지금 당장은 사용하지 않습니다.
- * 소통은 노션 페이지를 통해 이루어집니다.
- */
 
 @Configuration
 @OpenAPIDefinition(
         info = @Info(
-                title = "테스트 API 문서",
+                title = "EasyShift API 문서",
                 version = "1.0",
-                description = "API 문서입니다.",
-                contact = @Contact(
-                        name = "이열치열",
-                        url = "https://github.com/BurnToBurn-Devs ")),
-        security = @SecurityRequirement(name = "bearerAuth"))
-@SecurityScheme(
-        name = "bearerAuth",
-        type = SecuritySchemeType.HTTP,
-        scheme = "bearer",
-        bearerFormat = "JWT")
+                description = "EasyShift API 문서입니다.",
+                contact = @Contact(name = "이열치열", url = "https://github.com/BurnToBurn-Devs")
+        ),
+        servers = {
+                @Server(url = "https://api.easyshift.tech:8443", description = "Development Server"),
+                @Server(url = "https://api.easyshift.tech", description = "Production Server"),
+                @Server(url = "http://localhost:8080", description = "Local Server"),
+        },
+        security = @SecurityRequirement(name = "bearerAuth")
+)
+@SecurityScheme(name = "bearerAuth", type = SecuritySchemeType.HTTP, scheme = "bearer", bearerFormat = "JWT")
 public class SwaggerConfig {
-
+    
 }
