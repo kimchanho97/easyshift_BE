9e88cf4ca640885eeae7bcf595f8beeaddffb6e6
YeongJae0114 <yjl8628@naver.com>
2025-02-25 16:25:45 +0900

    feat: 조회 기능 구현 - JPQL

--- src/main/java/com/burntoburn/easyshift/repository/leave/LeaveRequestRepository.java
+++ src/main/java/com/burntoburn/easyshift/repository/leave/LeaveRequestRepository.java
@@ -1,9 +1,20 @@
 package com.burntoburn.easyshift.repository.leave;
 
 import com.burntoburn.easyshift.entity.leave.LeaveRequest;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LeaveRequestRepository extends JpaRepository<LeaveRequest, Long> {
+
+    // 특정 userId 의 모든 휴뮤일 조회
+    @Query("SELECT lr FROM LeaveRequest lr WHERE lr.user.id = :userId")
+    List<LeaveRequest> findAllByUserId(Long userId);
+
+    @Query("SELECT lr FROM LeaveRequest lr " +
+            "JOIN lr.schedule s " +
+            "WHERE s.scheduleMonth = :scheduleMonth")
+    List<LeaveRequest> findAllByScheduleMonth(String scheduleMonth);
 }
