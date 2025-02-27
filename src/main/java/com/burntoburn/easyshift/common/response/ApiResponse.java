bc9a98372d1c14ed986c174f399c858bac2f2315
kimchanho97 <nh0903@pusan.ac.kr>
2025-02-27 14:07:27 +0900

    feat: ApiResponse 정의

---
+++ src/main/java/com/burntoburn/easyshift/common/response/ApiResponse.java
@@ -0,0 +1,21 @@
+package com.burntoburn.easyshift.common.response;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class ApiResponse<T> {
+
+    private final boolean success;
+    private final T response;
+    private final ErrorResponse error;
+
+    public static <T> ApiResponse<T> success(T data) {
+        return new ApiResponse<>(true, data, null);
+    }
+
+    public static <T> ApiResponse<T> fail(ErrorResponse error) {
+        return new ApiResponse<>(false, null, error);
+    }
+}
