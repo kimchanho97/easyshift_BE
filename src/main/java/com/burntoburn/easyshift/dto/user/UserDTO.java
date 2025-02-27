0428c966b1ed63eacf92cc7b1aba5332c84da022
hojang <26dev@naver.com>
2025-02-27 15:54:51 +0900

    feat: 특정 매장의 유저 목록 조회

---
+++ src/main/java/com/burntoburn/easyshift/dto/user/UserDTO.java
@@ -0,0 +1,14 @@
+package com.burntoburn.easyshift.dto.user;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class UserDTO {
+    private Long id;
+    private String email;
+    private String phoneNumber;
+    private String avatarUrl;
+    private String role;
+}
