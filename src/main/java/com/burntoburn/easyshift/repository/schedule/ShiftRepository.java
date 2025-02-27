a4bcebb5b459285f9d86e702586a5192a8fbc2b6
YeongJae0114 <yjl8628@naver.com>
2025-02-28 00:54:20 +0900

    feat: fetch 조인으로 user 정보 조회

--- src/main/java/com/burntoburn/easyshift/repository/schedule/ShiftRepository.java
+++ src/main/java/com/burntoburn/easyshift/repository/schedule/ShiftRepository.java
@@ -3,9 +3,15 @@ package com.burntoburn.easyshift.repository.schedule;
 import com.burntoburn.easyshift.entity.schedule.Shift;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ShiftRepository extends JpaRepository<Shift, Long> {
     Optional<Shift> getShiftById(Long id);
+
+    // Shift 엔티티와 연결된 User 정보를 패치 조인
+    @Query("SELECT s FROM Shift s LEFT JOIN FETCH s.user WHERE s.id = :id")
+    Optional<Shift> findByIdWithUser(@Param("id") Long id);
 }
