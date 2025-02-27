9cb975882819ef63c0bc0518c584bd3bf74b6ddd
kimchanho97 <nh0903@pusan.ac.kr>
2025-02-27 14:02:36 +0900

    feat: User 엔티티 equals 구현

--- src/main/java/com/burntoburn/easyshift/entity/user/User.java
+++ src/main/java/com/burntoburn/easyshift/entity/user/User.java
@@ -4,6 +4,8 @@ import com.burntoburn.easyshift.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.Objects;
+
 @Getter
 @Entity
 @Table(name = "users") // user는 일부 DB에서 예약어이므로 users로 사용
@@ -32,13 +34,20 @@ public class User extends BaseEntity {
 
     private String avatarUrl;
 
-    public User update(String newEmail){
+    // 이름 변경하는 메서드
+    public void updateEmail(String newEmail) {
         this.email = newEmail;
-        return this;
     }
 
-    // 이름 변경하는 메서드
-    public void updateName(String newName) {
-        this.name = newName;
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(getId(), user.getId()) && Objects.equals(getEmail(), user.getEmail()) && Objects.equals(getName(), user.getName()) && Objects.equals(getPhoneNumber(), user.getPhoneNumber()) && getRole() == user.getRole() && Objects.equals(getAvatarUrl(), user.getAvatarUrl());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getEmail(), getName(), getPhoneNumber(), getRole(), getAvatarUrl());
     }
 }
