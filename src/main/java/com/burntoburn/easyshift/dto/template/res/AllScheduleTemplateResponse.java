b75d17c3c910646bf67aa734a87eb5ec04508d40
YeongJae0114 <yjl8628@naver.com>
2025-02-28 20:28:16 +0900

    feat: dto 생성 및 수정

--- src/main/java/com/burntoburn/easyshift/dto/template/res/AllScheduleTemplateResponse.java
+++ src/main/java/com/burntoburn/easyshift/dto/template/res/AllScheduleTemplateResponse.java
@@ -12,11 +12,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 public class AllScheduleTemplateResponse {
-    private List<ScheduleTemplateResponse> scheduleTemplateResponses;
+    private List<StoreTemplates> storeTemplateRespons;
 
     // ✅ 리스트 변환을 위한 정적 메서드 추가
     public static AllScheduleTemplateResponse fromEntityList(List<ScheduleTemplate> scheduleTemplates) {
-        List<ScheduleTemplateResponse> responseList = scheduleTemplates.stream()
+        List<StoreTemplates> responseList = scheduleTemplates.stream()
                 .map(ScheduleTemplate::toDTO) // 각 엔티티를 DTO로 변환
                 .toList();
         return new AllScheduleTemplateResponse(responseList);
--- src/main/java/com/burntoburn/easyshift/dto/template/res/StoreTemplates.java
+++ src/main/java/com/burntoburn/easyshift/dto/template/res/StoreTemplates.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class ScheduleTemplateResponse {
+public class StoreTemplates {
     private Long ScheduleTemplateId;
     private String scheduleTemplateName;
     private Long storeId;
