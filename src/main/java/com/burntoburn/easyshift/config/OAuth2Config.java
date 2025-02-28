6cdeace4bbc58d570a2f89f80a449694915f3e36
s1uth <handtaein@gmail.com>
2025-03-01 05:09:06 +0900

    feat: login 경로 수정

--- src/main/java/com/burntoburn/easyshift/config/OAuth2Config.java
+++ src/main/java/com/burntoburn/easyshift/config/OAuth2Config.java
@@ -1,8 +1,10 @@
 package com.burntoburn.easyshift.config;
 
+import com.burntoburn.easyshift.config.jwt.TokenAuthenticationFilter;
 import com.burntoburn.easyshift.config.jwt.TokenProvider;
 import com.burntoburn.easyshift.oauth2.handler.OAuth2FailureHandler;
 import com.burntoburn.easyshift.oauth2.handler.OAuth2SuccessHandler;
+import com.burntoburn.easyshift.oauth2.repository.OAuth2AuthorizationRequestRepository;
 import com.burntoburn.easyshift.oauth2.service.OAuth2UserCustomService;
 import com.burntoburn.easyshift.repository.user.RefreshTokenRepository;
 import com.burntoburn.easyshift.service.user.UserService;
@@ -10,15 +12,22 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.client.endpoint.DefaultAuthorizationCodeTokenResponseClient;
+import org.springframework.security.oauth2.client.endpoint.OAuth2AccessTokenResponseClient;
+import org.springframework.security.oauth2.client.endpoint.OAuth2AuthorizationCodeGrantRequest;
+import org.springframework.security.oauth2.client.endpoint.OAuth2AuthorizationCodeGrantRequestEntityConverter;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @RequiredArgsConstructor
 public class OAuth2Config {
+
     private TokenProvider tokenProvider;
     private final OAuth2UserCustomService oAuth2UserCustomService;
     private final RefreshTokenRepository refreshTokenRepository;
     private final UserService userService;
+    private final OAuth2AuthorizationRequestRepository oAuth2AuthorizationRequestRepository;
 
     @Bean
     public SecurityFilterChain oAuth2FilterChain(HttpSecurity http) throws Exception {
@@ -29,8 +38,11 @@ public class OAuth2Config {
                         .anyRequest().permitAll()
                 )
                 .oauth2Login(oauth2 -> {
-                    oauth2.loginPage("/login")
-                                .userInfoEndpoint(
+                    oauth2.authorizationEndpoint(
+                                    authEndPoint ->
+                                            authEndPoint.authorizationRequestRepository(
+                                                    oAuth2AuthorizationRequestRepository))
+                            .userInfoEndpoint(
                                     userInfoEndpointConfig ->
                                             userInfoEndpointConfig.userService(
                                                     oAuth2UserCustomService));
@@ -39,17 +51,25 @@ public class OAuth2Config {
                     oauth2.failureHandler(oAuth2FailureHandler());
                 }
         );
+        // token filter를 가장 먼저 실행함
+        http.addFilterBefore(tokenAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
 
+
     @Bean
     public OAuth2SuccessHandler oAuth2SuccessHanlder() {
-        return new OAuth2SuccessHandler(tokenProvider, refreshTokenRepository, userService);
+        return new OAuth2SuccessHandler(tokenProvider, refreshTokenRepository, oAuth2AuthorizationRequestRepository, userService);
     }
 
     @Bean
     public OAuth2FailureHandler oAuth2FailureHandler() {
         return new OAuth2FailureHandler();
+    }
+
+    @Bean
+    public TokenAuthenticationFilter tokenAuthenticationFilter() {
+        return new TokenAuthenticationFilter(tokenProvider);
     }
 }
