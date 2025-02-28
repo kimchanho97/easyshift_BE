4326c1f33dd50550975d52a612f95cfd2067462e
kimchanho97 <nh0903@pusan.ac.kr>
2025-03-01 05:13:58 +0900

    feat: StoreException 정의

---
+++ src/main/java/com/burntoburn/easyshift/exception/store/StoreErrorCode.java
@@ -0,0 +1,17 @@
+package com.burntoburn.easyshift.exception.store;
+
+import com.burntoburn.easyshift.common.exception.ErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@RequiredArgsConstructor
+@Getter
+public enum StoreErrorCode implements ErrorCode {
+
+    STORE_ACCESS_DENIED(HttpStatus.FORBIDDEN, 1001, "해당 매장에 대한 접근 권한이 없습니다.");
+
+    private final HttpStatus httpStatus;
+    private final int code;
+    private final String message;
+}
