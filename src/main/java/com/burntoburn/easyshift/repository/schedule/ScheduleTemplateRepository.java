e8626c3fc382e530afc3b34e938c6f4874a2c8a6
kimchanho97 <nh0903@pusan.ac.kr>
2025-03-01 05:17:08 +0900

    feat: 매장 조회 기능 구현

--- src/main/java/com/burntoburn/easyshift/repository/schedule/ScheduleTemplateRepository.java
+++ src/main/java/com/burntoburn/easyshift/repository/schedule/ScheduleTemplateRepository.java
@@ -1,12 +1,23 @@
 package com.burntoburn.easyshift.repository.schedule;
 
 import com.burntoburn.easyshift.entity.templates.ScheduleTemplate;
-import java.util.List;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ScheduleTemplateRepository extends JpaRepository<ScheduleTemplate, Long> {
-   List<ScheduleTemplate> findAllByStoreId(Long storeId);
+    List<ScheduleTemplate> findAllByStoreId(Long storeId);
+
+    @Query("""
+            SELECT st 
+            FROM ScheduleTemplate st
+            JOIN FETCH st.shiftTemplates shiftTemplates 
+            WHERE st.store.id = :storeId
+            """)
+    List<ScheduleTemplate> findAllWithShiftTemplatesByStoreId(@Param("storeId") Long storeId);
+
 }
