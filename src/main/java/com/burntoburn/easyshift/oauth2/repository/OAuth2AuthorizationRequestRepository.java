7f8a92ecde9545f6a31af0ab5a539e0bb7746400
s1uth <handtaein@gmail.com>
2025-03-01 12:18:24 +0900

    feat: loadAuthorizationRequest 수정

--- src/main/java/com/burntoburn/easyshift/oauth2/repository/OAuth2AuthorizationRequestRepository.java
+++ src/main/java/com/burntoburn/easyshift/oauth2/repository/OAuth2AuthorizationRequestRepository.java
@@ -1,12 +1,14 @@
 package com.burntoburn.easyshift.oauth2.repository;
 
 import com.burntoburn.easyshift.util.CookieUtil;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.oauth2.client.web.AuthorizationRequestRepository;
 import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.WebUtils;
 
 @RequiredArgsConstructor
 @Component  //OAuth2 인증 요청 정보를 쿠키에 저장, 로드, 삭제
@@ -17,9 +19,8 @@ public class OAuth2AuthorizationRequestRepository implements AuthorizationReques
 
     @Override // 카카오 인증 이후 이전의 요청을 다시 불러오는 메소드
     public OAuth2AuthorizationRequest loadAuthorizationRequest(HttpServletRequest request) {
-        return CookieUtil.getCookie(request, OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME)
-                .map(cookie -> CookieUtil.deserialize(cookie, OAuth2AuthorizationRequest.class))
-                .orElse(null);
+        Cookie cookie = WebUtils.getCookie(request, OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME);
+        return CookieUtil.deserialize(cookie, OAuth2AuthorizationRequest.class);
 
     }
     @Override  //최초에 하이퍼 링크를 통해 온 요청을 저장
