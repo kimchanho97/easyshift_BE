fcd28de082ef2b67f4b2a72d0f2639f83c8a0666
YeongJae0114 <yjl8628@naver.com>
2025-03-02 13:29:57 +0900

    feat: ShiftTemplate 생성 시 ScheduleTemplate 관계 주입

--- src/main/java/com/burntoburn/easyshift/service/templates/ScheduleTemplateFactory.java
+++ src/main/java/com/burntoburn/easyshift/service/templates/ScheduleTemplateFactory.java
@@ -23,18 +23,20 @@ public class ScheduleTemplateFactory {
                 .build();
 
         // ✅ ShiftTemplate 리스트를 생성하는 별도 메서드 활용
-        List<ShiftTemplate> shiftTemplates = createShiftTemplates(request.getShiftTemplates());
+        List<ShiftTemplate> shiftTemplates = createShiftTemplates(scheduleTemplate, request.getShiftTemplates());
         shiftTemplates.forEach(scheduleTemplate::addShiftTemplate);
 
         return scheduleTemplate;
     }
 
     // ✅ ShiftTemplate 리스트 생성 메서드 추가
-    public List<ShiftTemplate> createShiftTemplates(List<ShiftTemplateRequest> shiftTemplateRequests) {
+    public List<ShiftTemplate> createShiftTemplates(ScheduleTemplate scheduleTemplate, List<ShiftTemplateRequest> shiftTemplateRequests) {
         return Optional.ofNullable(shiftTemplateRequests)
                 .orElse(Collections.emptyList()) // null 방지
                 .stream()
+                // ScheduleTemplate 연관관계 주입
                 .map(shiftRequest -> ShiftTemplate.builder()
+                        .scheduleTemplate(scheduleTemplate)
                         .shiftTemplateName(shiftRequest.getShiftName())
                         .startTime(shiftRequest.getStartTime())
                         .endTime(shiftRequest.getEndTime())
