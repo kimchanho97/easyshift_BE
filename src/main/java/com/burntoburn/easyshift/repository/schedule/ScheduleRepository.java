8285c864af83e75a5e6f1c63a19c768368faadeb
YeongJae0114 <yjl8628@naver.com>
2025-03-04 17:46:40 +0900

    feat: JPQL로 특정 조건 조회 개발

--- src/main/java/com/burntoburn/easyshift/repository/schedule/ScheduleRepository.java
+++ src/main/java/com/burntoburn/easyshift/repository/schedule/ScheduleRepository.java
@@ -1,6 +1,9 @@
 package com.burntoburn.easyshift.repository.schedule;
 
 import com.burntoburn.easyshift.entity.schedule.Schedule;
+import java.time.YearMonth;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -17,8 +20,35 @@ public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
     // 특정 매장의 특정 스케줄 조회 (storeId 검증 포함)
     Optional<Schedule> findByIdAndStoreId(Long scheduleId, Long storeId);
 
-    // 일급 컬렉션 제거
-    @Query("SELECT s FROM Schedule s LEFT JOIN FETCH s.shifts WHERE s.id = :scheduleId")
-    Optional<Schedule> findByIdWithShifts(@Param("scheduleId") Long scheduleId);
+    // 스케줄 조회(scheduleId 로)
+    @Query("""
+            SELECT s FROM Schedule s 
+            LEFT JOIN FETCH s.shifts sh
+            LEFT JOIN FETCH sh.user u 
+            WHERE s.id = :scheduleId
+            """)
+    Optional<Schedule> findScheduleWithShifts(@Param("scheduleId") Long scheduleId);
 
+    Page<Schedule> findByStoreIdOrderByCreatedAtDesc(Long storeId, Pageable pageable);
+
+    @Query("""
+    SELECT s FROM Schedule s
+    JOIN FETCH s.store st
+    JOIN FETCH s.shifts sh
+    JOIN sh.user u
+    WHERE st.id = :storeId
+    AND s.scheduleMonth = :scheduleMonth
+    AND u.id = :userId
+    """)
+    List<Schedule> findWorkerSchedules(
+            @Param("storeId") Long storeId,
+            @Param("scheduleMonth") YearMonth scheduleMonth,
+            @Param("userId") Long userId
+    );
+
+    @Query("""
+    SELECT s FROM Schedule s
+    WHERE s.scheduleTemplateId = :scheduleTemplateId
+    """)
+    List<Schedule>findSchedulesWithTemplate(Long scheduleTemplateId);
 }
--- src/main/java/com/burntoburn/easyshift/repository/schedule/ScheduleTemplateRepository.java
+++ src/main/java/com/burntoburn/easyshift/repository/schedule/ScheduleTemplateRepository.java
@@ -1,6 +1,7 @@
 package com.burntoburn.easyshift.repository.schedule;
 
 import com.burntoburn.easyshift.entity.templates.ScheduleTemplate;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -10,6 +11,7 @@ import java.util.List;
 
 @Repository
 public interface ScheduleTemplateRepository extends JpaRepository<ScheduleTemplate, Long> {
+
     List<ScheduleTemplate> findAllByStoreId(Long storeId);
 
     @Query("""
@@ -20,4 +22,13 @@ public interface ScheduleTemplateRepository extends JpaRepository<ScheduleTempla
             """)
     List<ScheduleTemplate> findAllWithShiftTemplatesByStoreId(@Param("storeId") Long storeId);
 
+    // 스케줄 템플릿 ID로 단일 스케줄 템플릿 조회 (ShiftTemplates 포함)
+    @Query("""
+            SELECT st 
+            FROM ScheduleTemplate st
+            JOIN FETCH st.shiftTemplates shiftTemplates
+            WHERE st.id = :scheduleTemplateId
+            """)
+    Optional<ScheduleTemplate> findScheduleTemplateWithShiftsById(@Param("scheduleTemplateId") Long scheduleTemplateId);
 }
+
