0f38af5311a4491b0c955566451b44f9daeaaa31
YeongJae0114 <yjl8628@naver.com>
2025-03-06 19:05:18 +0900

    feat: 예외 추가

--- src/main/java/com/burntoburn/easyshift/exception/schedule/ScheduleErrorCode.java
+++ src/main/java/com/burntoburn/easyshift/exception/schedule/ScheduleErrorCode.java
@@ -9,7 +9,10 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum ScheduleErrorCode implements ErrorCode {
     SCHEDULE_NOT_FOUND(HttpStatus.NOT_FOUND, 1001, "해당 스케줄을 찾을 수 없습니다."),
-    INSUFFICIENT_USERS_FOR_ASSIGNMENT(HttpStatus.BAD_REQUEST, 1002, "자동 배정을 수행하기에 근무 가능한 사용자가 부족합니다.");
+    INSUFFICIENT_USERS_FOR_ASSIGNMENT(HttpStatus.BAD_REQUEST, 1002, "자동 배정을 수행하기에 근무 가능한 사용자가 부족합니다."),
+
+    SCHEDULE_PAGE_NOT_FOUND(HttpStatus.NOT_FOUND, 2002, "요청한 페이지에 스케줄이 존재하지 않습니다.");
+
 
     private final HttpStatus httpStatus;
     private final int code;
