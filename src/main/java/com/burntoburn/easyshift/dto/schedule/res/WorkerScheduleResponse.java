7e2355ff87bfb898c76fd1885282a53c01679976
YeongJae0114 <yjl8628@naver.com>
2025-03-09 15:06:10 +0900

    feat: dto 에 shiftTemplateName 필드 추가

--- src/main/java/com/burntoburn/easyshift/dto/schedule/res/WorkerScheduleResponse.java
+++ src/main/java/com/burntoburn/easyshift/dto/schedule/res/WorkerScheduleResponse.java
@@ -3,6 +3,7 @@ package com.burntoburn.easyshift.dto.schedule.res;
 import com.burntoburn.easyshift.entity.schedule.Schedule;
 import com.burntoburn.easyshift.entity.store.Store;
 import java.util.List;
+import java.util.Map;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,9 +15,9 @@ public class WorkerScheduleResponse {
     private StoreInfo store;
     private List<WorkerSchedule> schedules;
 
-    public static WorkerScheduleResponse fromEntity(Store store, List<Schedule> schedules) {
+    public static WorkerScheduleResponse fromEntity(Store store, List<Schedule> schedules, Map<Long, String> scheduleIdToTemplateNameMap) {
         List<WorkerSchedule> scheduleDTOs = schedules.stream()
-                .map(WorkerSchedule::fromEntity)
+                .map(schedule -> WorkerSchedule.fromEntity(schedule, scheduleIdToTemplateNameMap))
                 .toList();
         StoreInfo storeInfo = StoreInfo.fromEntity(store);
         return new WorkerScheduleResponse(storeInfo, scheduleDTOs);
