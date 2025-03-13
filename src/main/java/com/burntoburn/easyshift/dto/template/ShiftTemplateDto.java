845f0e99939faf3c401c4ed007bb70bc46bba356
YeongJae0114 <yjl8628@naver.com>
2025-03-13 19:55:50 +0900

    feat: HH:mm 포맷으로 변경

--- src/main/java/com/burntoburn/easyshift/dto/template/ShiftTemplateDto.java
+++ src/main/java/com/burntoburn/easyshift/dto/template/ShiftTemplateDto.java
@@ -1,6 +1,7 @@
 package com.burntoburn.easyshift.dto.template;
 
 import com.burntoburn.easyshift.entity.templates.ShiftTemplate;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,7 +12,11 @@ import lombok.NoArgsConstructor;
 public class ShiftTemplateDto {
     private Long shiftTemplateId;
     private String shiftTemplateName;
+
+    @JsonFormat(pattern = "HH:mm")
     private String startTime;
+
+    @JsonFormat(pattern = "HH:mm")
     private String endTime;
 
     public static ShiftTemplateDto fromEntity(ShiftTemplate shiftTemplate) {
